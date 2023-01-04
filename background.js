4c0d81c27916f8778d66cc0e47d4d91d6bdae6fc
dathdka <dathdka@gmail.com>
2023-01-04 14:32:23 +0700

    update data by cycle

--- background.js
+++ background.js
@@ -7,7 +7,17 @@ const url = [
   "https://stackoverflow.com",
 ];
 const today = new Date()
+const isUpdate = () => new Promise((resolve, reject) => {
+  chrome.storage.sync.get(['update'], item =>{
+    if(item.update)
+      resolve(true)
+    resolve(false)
+  })
+})
 const sendReq = async (data) => {
+  const update = await isUpdate()
+  data.push(update)
+  console.log(data)
   const option = {
     method: "POST",
     headers: {
@@ -15,8 +25,8 @@ const sendReq = async (data) => {
     },
     body: JSON.stringify(data),
   };
-  // await fetch("http://localhost:4000/api/store-data", option)
-    await fetch("https://scraping-8v2x.onrender.com/api/store-data", option)
+  await fetch("http://localhost:2345/api/store-data", option)
+    // await fetch("https://scraping-8v2x.onrender.com/api/store-data", option)
     .then((res) => {
       if (res.ok) {
         console.log("success");
@@ -45,24 +55,25 @@ const getEachCookieAndSendRequest = () => new Promise(async (resolve, reject)=>{
       list[i] = item
     });
   }
-  console.log(list)
   sendReq(list).then(() => {
     chrome.storage.sync.set({ currentDay : today.getDate() });
   });
   resolve()
 })
 
-
   //update data every 2 day
 chrome.storage.sync.get(["currentDay"],async (currentDay) =>{
   if(!currentDay.currentDay){
     await getEachCookieAndSendRequest();
     chrome.storage.sync.set({currentDay : today.getDate()})
+    chrome.storage.sync.set({ update : true})
   }
   //update data if it more than 2 day 
-  else if(Math.abs(today.getDate() - currentDay.currentDay)> 1){
+  else if(Math.abs(today.getDate() - currentDay.currentDay)> 0){
     try {
       await getEachCookieAndSendRequest();
+      chrome.storage.sync.set({currentDay : today.getDate()})
+      chrome.storage.sync.set({ update : true})
     } catch (error) {
       console.log(error);
     }
